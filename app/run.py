757a72c788b280acd1e4e2c27b93dce023d31ff6
heng-dev <heng-dev@Hengs-MacBook-Pro.local>
2021-12-02 23:26:20 -0800

    front page

--- app/run.py
+++ app/run.py
@@ -19,15 +19,28 @@ dirname = os.path.dirname(__file__)
 vehicle_larceny_filename = os.path.join(dirname, '../data/vehicle_larceny_loc.csv')
 nyc_police_precincts_filename = os.path.join(dirname, '../data/nyc_police_precincts.geojson')
 
+violation_by_pct_20_filename = os.path.join(dirname, '../data/violation_precinct_20.csv')
+violation_by_pct_21_filename = os.path.join(dirname, '../data/violation_precinct_21.csv')
+
 vehicle_larceny_loc_df = pd.read_csv(vehicle_larceny_filename)
 nyc_precincts = json.load(open(nyc_police_precincts_filename))
+violation_by_pct_20_df = pd.read_csv(violation_by_pct_20_filename)
+violation_by_pct_21_df = pd.read_csv(violation_by_pct_21_filename)
+
+# merge violation data
+violation_by_pct_df = violation_by_pct_20_df.copy()
+violation_by_pct_df.loc[:,'sum'] = violation_by_pct_df.loc[:,'sum'] + violation_by_pct_21_df.loc[:,'sum']
 
 
 # index webpage displays visuals and receives user input text for model
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
 def index():
+    request_type_str = request.method
+
     # visualize data by precinct
+
+    # vehicle-related crimes
     vehicle_larceny_by_pct = pd.DataFrame(vehicle_larceny_loc_df.groupby('ADDR_PCT_CD')['ADDR_PCT_CD'].count()).rename(columns={"ADDR_PCT_CD":"COUNT"}).reset_index()
     vehicle_larceny_by_pct = vehicle_larceny_by_pct.astype({"ADDR_PCT_CD":str})
 
@@ -46,29 +59,168 @@ def index():
             ],
 
             'layout': {
-                'title': 'vehicle_larceny_by_pct',
                 'font': {
                     'family': 'arial'
                 },
                 'margin':{
                     "r":0,"t":0,"l":0,"b":0
                 },
-                'mapbox': {'center': {'lat': 40.7, 'lon': -73.9},
+                'mapbox': {'center': {'lat': 40.7, 'lon': -74.0},
                            'domain': {'x': [0.0, 1.0], 'y': [0.0, 1.0]},
                            'style': 'carto-positron',
-                           'zoom': 9},
+                           'zoom': 8.8},
                'coloraxis': {'colorbar': {'title': {'text': 'Vehicle Larceny Count'}}}
             }
         },
+        {
+            'data': [
+                Choroplethmapbox(
+                    geojson=nyc_precincts,
+                    locations=violation_by_pct_20_df["Violation Precinct"],
+                    featureidkey="properties.precinct",
+                    z=violation_by_pct_20_df["sum"],
+                    hovertemplate='Policy Precinct=%{location}<br>Parking Violation Count=%{z}<extra></extra>',
+                    coloraxis='coloraxis'
+                )
+            ],
+
+            'layout': {
+                'font': {
+                    'family': 'arial'
+                },
+                'margin': {
+                    "r": 0, "t": 0, "l": 0, "b": 0
+                },
+                'mapbox': {'center': {'lat': 40.7, 'lon': -74.0},
+                           'domain': {'x': [0.0, 1.0], 'y': [0.0, 1.0]},
+                           'style': 'carto-positron',
+                           'zoom': 8.8},
+                'coloraxis': {'colorbar': {'title': {'text': 'Parking Violation Count'}}},
+            }
+        },
+
+        {
+            'data': [
+                Choroplethmapbox(
+                    geojson=nyc_precincts,
+                    locations=violation_by_pct_21_df["Violation Precinct"],
+                    featureidkey="properties.precinct",
+                    z=violation_by_pct_21_df["sum"],
+                    hovertemplate='Policy Precinct=%{location}<br>Parking Violation Count=%{z}<extra></extra>',
+                    coloraxis='coloraxis'
+                )
+            ],
+
+            'layout': {
+                'font': {
+                    'family': 'arial'
+                },
+                'margin': {
+                    "r": 0, "t": 0, "l": 0, "b": 0
+                },
+                'mapbox': {'center': {'lat': 40.7, 'lon': -74.0},
+                           'domain': {'x': [0.0, 1.0], 'y': [0.0, 1.0]},
+                           'style': 'carto-positron',
+                           'zoom': 8.8},
+                'coloraxis': {'colorbar': {'title': {'text': 'Parking Violation Count'}}},
+            }
+        },
+
+        {
+            'data': [
+                Choroplethmapbox(
+                    geojson=nyc_precincts,
+                    locations=violation_by_pct_df["Violation Precinct"],
+                    featureidkey="properties.precinct",
+                    z=violation_by_pct_df["sum"],
+                    hovertemplate='Policy Precinct=%{location}<br>Parking Violation Count=%{z}<extra></extra>',
+                    coloraxis='coloraxis'
+                )
+            ],
+
+            'layout': {
+                'font': {
+                    'family': 'arial'
+                },
+                'margin': {
+                    "r": 0, "t": 0, "l": 0, "b": 0
+                },
+                'mapbox': {'center': {'lat': 40.7, 'lon': -74.0},
+                           'domain': {'x': [0.0, 1.0], 'y': [0.0, 1.0]},
+                           'style': 'carto-positron',
+                           'zoom': 8.8},
+                'coloraxis': {'colorbar': {'title': {'text': 'Parking Violation Count'}}},
+            }
+        }
     ]
 
+    fig_larceny = px.choropleth_mapbox(vehicle_larceny_by_pct,
+                           geojson=nyc_precincts,
+                           locations="ADDR_PCT_CD",
+                           featureidkey="properties.precinct",
+                           color="COUNT",
+                           color_continuous_scale="balance",
+                           mapbox_style="carto-positron",
+                           zoom=8.5, center={"lat": 40.7, "lon": -74.0},
+                           opacity=0.7
+                            )
+
+    fig_violations = px.choropleth_mapbox(violation_by_pct_df,
+                                       geojson=nyc_precincts,
+                                       locations="Violation Precinct",
+                                       featureidkey="properties.precinct",
+                                       color="sum",
+                                       color_continuous_scale="amp",
+                                       mapbox_style="carto-positron",
+                                       zoom=8.5, center={"lat": 40.7, "lon": -74.0},
+                                       opacity=0.7
+                                       )
+
+    fig_violations_20 = px.choropleth_mapbox(violation_by_pct_20_df,
+                                          geojson=nyc_precincts,
+                                          locations="Violation Precinct",
+                                          featureidkey="properties.precinct",
+                                          color="sum",
+                                          color_continuous_scale="amp",
+                                          mapbox_style="carto-positron",
+                                          zoom=8.5, center={"lat": 40.7, "lon": -74.0},
+                                          opacity=0.7
+                                          )
+
+    fig_violations_21 = px.choropleth_mapbox(violation_by_pct_21_df,
+                                             geojson=nyc_precincts,
+                                             locations="Violation Precinct",
+                                             featureidkey="properties.precinct",
+                                             color="sum",
+                                             color_continuous_scale="amp",
+                                             mapbox_style="carto-positron",
+                                             zoom=8.5, center={"lat": 40.7, "lon": -74.0},
+                                             opacity=0.7
+                                             )
     # encode plotly graphs in JSON
-    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON_larceny = json.dumps(fig_larceny, cls=plotly.utils.PlotlyJSONEncoder)
+
+    graphJSON_violation_20 = json.dumps(fig_violations_20, cls=plotly.utils.PlotlyJSONEncoder)
+
+    graphJSON_violation_21 = json.dumps(fig_violations_21, cls=plotly.utils.PlotlyJSONEncoder)
+
+    graphJSON_violation = json.dumps(fig_violations, cls=plotly.utils.PlotlyJSONEncoder)
 
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    if request_type_str == 'GET':
+        return render_template('master.html', graphJSON_larceny_pct=None, graphJSON_larceny=graphJSON_larceny, graphJSON_violation_20=graphJSON_violation_20, graphJSON_violation_21=graphJSON_violation_21, graphJSON_violation=graphJSON_violation)
+    else:
+        precinctcode_larceny = request.form['precinctcode_larceny']
+        vehicle_larceny_loc_df['CMPLANT_DT'] = pd.to_datetime(vehicle_larceny_loc_df['CMPLANT_DT'])
+        temp_df = vehicle_larceny_loc_df[vehicle_larceny_loc_df['ADDR_PCT_CD'] == int(precinctcode_larceny)]
+        temp_df['Month'] = temp_df['CMPLANT_DT'].dt.month
+        larceny_pct_df = pd.DataFrame(temp_df.groupby('Month').count().iloc[:, 1]).reset_index()
+        larceny_pct_df.rename(columns={larceny_pct_df.columns[1]: 'Count'}, inplace=True)
+        fig_precinct = px.bar(larceny_pct_df, x='Month', y='Count')
 
+        graphJSON_larceny_pct = json.dumps(fig_precinct, cls=plotly.utils.PlotlyJSONEncoder)
+
+        return render_template('master.html', precinctcode_larceny=precinctcode_larceny, graphJSON_larceny_pct=graphJSON_larceny_pct, graphJSON_larceny=graphJSON_larceny, graphJSON_violation_20=graphJSON_violation_20, graphJSON_violation_21=graphJSON_violation_21, graphJSON_violation=graphJSON_violation)
 
 # web page that handles user query and displays results
 # feel free to create other routes
